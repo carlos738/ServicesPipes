1df4c11db9c9a91fa02815bf43d5b0538a2a2f28
carlos738 <carlosasl135@hotmail.com>
2022-12-02 19:50:48 -0300

    C10 angular service pipes

--- SERVICES-PIPES/C10/src/app/app.module.ts
+++ SERVICES-PIPES/C10/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TransformadorPipe } from './pipes/transformador.pipe';
+import { CardComponent } from './components/card/card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    TransformadorPipe,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

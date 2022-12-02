2a162b841f02956eea8089b9684056b4da34ec38
carlos738 <carlosasl135@hotmail.com>
2022-12-02 03:11:18 -0300

    service-pipes

--- pipes-service/src/app/components/card/card.component.ts
+++ pipes-service/src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+name:string ="charizard"
+attributesTypes:string[]=['FIRE','ROCK']
 
-  constructor() { }
+  constructor(
+    private service:PokemonService
+  ) { 
+    this.pokemon ={
+      id:0, name: '',
+      sprites:{
+        front_default: ''
+      },types:[]
+    }
+  }
 
   ngOnInit(): void {
+    this.getPokemon('pikachu')
+  }
+  getPokemon(searchName:string){
+    this.service.getPokemon(searchName).subscribe({
+      next: (res)=>{
+        this.pokemon ={
+          id: res.id,
+          name: res.name,
+          sprites: res.sprites,
+          types: res.types
+        }
+      },
+      error: (err) => console.log('not found')
+      
+    }
+    )
   }
 
 }

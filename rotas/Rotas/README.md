d503f251167d33d914186f62659fdb2da03ead60
carlos738 <carlosasl135@hotmail.com>
2022-12-02 20:07:38 -0300

    C10

--- rotas/Rotas/README.md
+++ rotas/Rotas/README.md
@@ -1,4 +1,4 @@
-# ServicePipes
+# Rotas
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.5.
 
--- rotas/Rotas/src/app/app.component.spec.ts
+++ rotas/Rotas/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'service-pipes'`, () => {
+  it(`should have as title 'Rotas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('service-pipes');
+    expect(app.title).toEqual('Rotas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('service-pipes app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Rotas app is running!');
   });
 });
